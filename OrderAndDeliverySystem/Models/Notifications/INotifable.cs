b6aec8f72f42bfc69770f04b85776f80f5633a55
Shawn Lee <100383590+ShawnLeeyz@users.noreply.github.com>
2026-05-20 18:10:01 +1200

    Delete OrderAndDeliverySystem directory

--- OrderAndDeliverySystem/Models/Notifications/INotifable.cs
+++
@@ -1,7 +0,0 @@
-namespace OrderAndDeliverySystem.Models.Notifications
-{   
-    // Interface for any class that can receive notifications
-    public interface INotifable
-    {        void triggerNotification(Notification notification);
-    }
-}
--- OrderAndDeliverySystem/Models/Notifications/NotifcationType.cs
+++
@@ -1,9 +0,0 @@
-namespace OrderAndDeliverySystem.Models.Notifications
-{
-    // Enum to represent different types of notifications.
-    // Currently only IN_APP but can be expanded to EMAIL, SMS etc in future
-    public enum NotificationType
-    {
-        IN_APP
-    }
-}
